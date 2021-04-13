eac360bd5f0327d2f853f8e95db7d9a5d557afc4
Jake Westling <jakewestling@gmail.com>
2021-04-13 11:51:22 -0400

    UI Only.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// Function Based Component
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+
+const App = () => {
+  // const name = "Brad"
+  // const x = false
+  const [showAddTask, setShowAddTask] = useState(true)
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    }
+  ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 100000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Could use empty tag set if div not wanted around everything. <> </>
+    <div className="container">
+      <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ?
+        (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
+
+      {/* <h1>Hello</h1>
+      <h2>Hi {x ? 'Yes' : 'No'}</h2> */}
     </div>
   );
 }
+
+// Class based Component
+// import React from "react";
+// import Header from "./components/Header";
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hi from a class based component</h1>;
+//   }
+// }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className='header'>
+
+      {/* Using styles inline */}
+      {/* <h1 style={{ color: 'red', backgroundColor: 'aliceblue' }}>{title}</h1> */}
+      {/* Using styles from JS object headingStyle below */}
+      {/* <h1 style={headingStyle}>{title}</h1> */}
+
+      <h1>{title}</h1>
+      {/* <Button color='dodgerblue' text='Hello 2' />
+      <Button color='gold' text='Hello 3' /> */}
+      <Button
+        color={showAdd ? 'red': 'limegreen'}
+        text={showAdd ? 'Minimize' : 'Create Task'}
+        onClick={onAdd}
+      />
+
+    </header>
+  )
+}
+
+// Set default values of properties here, if needed
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+// Set the property types for error checking here
+// or use typescript
+// can also set required boolean
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'aliceblue'
+
+// }
+
+
+export default Header
